2e6f6fe7565aca9b7483c0f37d51aadb44129275
JuanGLopezM <105565395+JuanGLopezM@users.noreply.github.com>
2022-07-13 21:26:24 +0200

    iteración 2 completada

--- src/App.js
+++ src/App.js
@@ -5,18 +5,41 @@ import { useState } from "react";
 
 
 
-
+const first5Contacts = contacts.splice(0, 5);
 
 function App() {
-  const [contactList, setContactList] = useState(contacts.slice(0, 5))
-  return <div className="App">
-  <table>  
-        {contactList.map((pop) => {
-          return < pop.picture pop.name pop.popularity/>;
-        })}    
-
-  </table>
-  </div>;
+  const [contactList, setContactList] = useState(first5Contacts)
+  return (
+  <div className="App">
+    <table className="tableActors">  
+      <thead>
+        <tr>
+          <th> Picture </th>
+          <th> Name </th>
+          <th> Popularity </th>
+        </tr>
+      </thead>
+      <tbody>
+          {contactList.map((actor) => {
+            return (
+              <tr key={actor.id}>
+                <td>
+                  <img
+                    src={actor.pictureUrl}
+                    width="100px"
+                    height="120px"
+                    alt="Actor"
+                  />
+                </td>
+                <td> {actor.name} </td>
+                <td> {Number(actor.popularity).toFixed(2)}</td>
+              </tr>
+            );
+          })}
+      </tbody>  
+    </table>
+  </div>
+  )
 }
 export default App;
 
