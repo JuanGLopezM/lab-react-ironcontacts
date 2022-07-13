f25611c6445a11bb1d7b3b65ae65c3904aee8a1f
JuanGLopezM <105565395+JuanGLopezM@users.noreply.github.com>
2022-07-13 23:37:33 +0200

    iteración 5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // src/App.js
+import React, { useState } from 'react';
 import "./App.css";
 import contacts from "./contacts.json";
-import { useState } from "react";
+import Button from 'react-bootstrap/Button';
 
 
 
@@ -9,14 +10,49 @@ const first5Contacts = contacts.splice(0, 5);
 
 function App() {
   const [contactList, setContactList] = useState(first5Contacts)
+  let copyArr = [...contactList]
+
+  const addRandomContact = () => {   
+    let randomContact =
+      contacts[Math.floor(Math.random() * contacts.length) + 5];
+    let existIn = contacts.indexOf(randomContact);
+    if (randomContact) {
+      copyArr.unshift(randomContact);
+    }
+    if (existIn > -1) {
+      contacts.splice(existIn, 1);
+    }
+    setContactList(copyArr);
+  };
+
+  const sortByPopularity = () => {
+    copyArr.sort(function (a, b) {
+      return b.popularity - a.popularity;
+    });
+    setContactList(copyArr);
+  };
+  const sortByName = () => {
+    copyArr.sort((a, b) => a.name.localeCompare(b.name));
+    setContactList(copyArr);
+  };
+
+  const deleteContact = (actorID) => {
+    setContactList(contactList.filter((c) => c !== actorID));
+  };
+
   return (
   <div className="App">
+  <Button className="buttons" onClick={addRandomContact}>Add random contact</Button>
+  <Button className="buttons" onClick={sortByName}>Sort by name</Button>
+  <Button className="buttons" onClick={sortByPopularity}>Sort by popularity</Button>
     <table className="tableActors">  
       <thead>
         <tr>
           <th> Picture </th>
           <th> Name </th>
           <th> Popularity </th>
+          <th> Won Oscar</th>
+          <th> Won Emmy</th>
         </tr>
       </thead>
       <tbody>
@@ -33,6 +69,19 @@ function App() {
                 </td>
                 <td> {actor.name} </td>
                 <td> {Number(actor.popularity).toFixed(2)}</td>
+                <td>{actor.wonOscar ? "🏆" : ""}</td>
+                <td>{actor.wonEmmy ? "🏆" : ""}</td>
+                <td>
+                  <Button
+                    variant="outline-danger"
+                    onClick={() => {
+                      deleteContact(actor); 
+                    }}
+                    id={actor.id}
+                  >
+                    Remove
+                  </Button>
+                </td>
               </tr>
             );
           })}
